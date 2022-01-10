706f85b1341abbfea6dd15b3f74fdb5c6c9dfdb6
Ray Hancock <rhancock@attune.gshapps.net>
2022-01-10 12:35:41 -0500

    Add project files.

---
+++ GameRoot.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace HexGame
+{
+    public class GameRoot : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private GraphicsDevice _device;
+        private SpriteBatch _spriteBatch;
+
+        private int _screenWidth;
+        private int _screenHeight;
+
+        public GameRoot()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 500;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.IsFullScreen = false;
+            _graphics.ApplyChanges();
+
+            Window.Title = "HexGame";
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _device = _graphics.GraphicsDevice;
+
+            _screenWidth = _device.PresentationParameters.BackBufferWidth;
+            _screenHeight = _device.PresentationParameters.BackBufferHeight;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HexGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameRoot())
+                game.Run();
+        }
+    }
+}
